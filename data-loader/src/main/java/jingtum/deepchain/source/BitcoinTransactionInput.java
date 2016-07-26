a7abafc48ea17373d000988608df4b9ab84a5b7a
mac <zjsynce@sjtu.org>
2016-07-26 18:05:23 +0800

    Second commit, classes for transaction data.

--- data-loader/src/main/java/jingtum/deepchain/source/BitcoinTransactionInput.java
+++ data-loader/src/main/java/jingtum/deepchain/source/BitcoinTransactionInput.java
@@ -1,7 +1,49 @@
 package jingtum.deepchain.source;
 
+import java.io.Serializable;
+
 /**
- * Created by mac on 16/7/26.
+ * Created by nichunen on 16/7/26.
  */
-public class BitcoinTransactionInput {
+public class BitcoinTransactionInput implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 283893453089295979L;
+
+    private byte[] prevTransactionHash;
+    private long previousTxOutIndex;
+    private byte[] txInScriptLength;
+    private byte[] txInScript;
+    private long seqNo;
+
+    public BitcoinTransactionInput(byte[] prevTransactionHash, long previousTxOutIndex, byte[] txInScriptLength, byte[] txInScript, long seqNo) {
+        this.prevTransactionHash=prevTransactionHash;
+        this.previousTxOutIndex=previousTxOutIndex;
+        this.txInScriptLength=txInScriptLength;
+        this.txInScript=txInScript;
+        this.seqNo=seqNo;
+    }
+
+    public byte[] getPrevTransactionHash() {
+        return this.prevTransactionHash;
+    }
+
+    public long getPreviousTxOutIndex() {
+        return this.previousTxOutIndex;
+    }
+
+    public byte[] getTxInScriptLength() {
+        return this.txInScriptLength;
+    }
+
+    public byte[] getTxInScript() {
+        return this.txInScript;
+    }
+
+    public long getSeqNo() {
+        return this.seqNo;
+    }
+
 }
--- data-loader/src/main/java/jingtum/deepchain/source/BitcoinTransactionOutput.java
+++ data-loader/src/main/java/jingtum/deepchain/source/BitcoinTransactionOutput.java
@@ -1,7 +1,37 @@
 package jingtum.deepchain.source;
 
+import java.io.Serializable;
+
 /**
  * Created by mac on 16/7/26.
  */
-public class BitcoinTransactionOutput {
+public class BitcoinTransactionOutput implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 2854570630540937753L;
+
+    private long value;
+    private byte[] txOutScriptLength;
+    private byte[] txOutScript;
+
+    public BitcoinTransactionOutput(long value, byte[] txOutScriptLength, byte[] txOutScript) {
+        this.value=value;
+        this.txOutScriptLength=txOutScriptLength;
+        this.txOutScript=txOutScript;
+    }
+
+    public long getValue() {
+        return this.value;
+    }
+
+    public byte[] getTxOutScriptLength() {
+        return this.txOutScriptLength;
+    }
+
+    public byte[] getTxOutScript() {
+        return this.txOutScript;
+    }
+
 }
